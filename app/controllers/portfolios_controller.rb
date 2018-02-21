279b03de7efa10841858fd05ef7f43414ad3c004
Tim Pile <tim@gritchat.com>
2018-02-21 12:28:31 -0500

    Implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,6 +37,13 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully deleted.' }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
